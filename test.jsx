1786dc08eb1f944acea7b29a4472a68422412825
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-06-22 16:33:20 +0800

    Finish bh-mj-focus-detail component

---
+++ test.jsx
@@ -0,0 +1,9 @@
+ 
+"use strict"; 
+ 
+import React from "react"; 
+import ReactDOM from "react-dom"; 
+ 
+import FocusDetail from "./focus-detail.js"; 
+ 
+ReactDOM.render( <FocusDetail></FocusDetail> ); 
